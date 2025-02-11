0ab6e399c115e8615454831438662bb609eaa5f8
unknown <babatundebolu@gmail.com>
2025-02-11 07:44:17 -0800

    Initial commit

---
+++ JSTACK-Job-Portal-Website-main/send_email.php
@@ -0,0 +1,26 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "babatundebolu@gmail.com";
+    $subject = "New Contcat Form";
+    $headers = 'From: ' . $name . "<" . $email . ">"\r\n;
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "Content-Type: text/plain; charset-8\r\n"
+
+    if(mail($to, $subject, $message, $headers)){
+        http_response_code(200);
+        echo "email send successfully!";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong";
+    } else {
+        http_response_code(403);
+        echo "Access Denied";
+    }
+}
+
+?>
